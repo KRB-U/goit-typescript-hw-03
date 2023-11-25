6174e51142aee26bf0a464be74dc210227657951
unknown <rubikonspace@ukr.net>
2023-11-25 22:52:25 +0200

    3

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-"use strict";
 class Rectangle {
     constructor(width, height) {
         this.width = width;
@@ -10,4 +9,88 @@ class Rectangle {
 }
 const calcRectangle = new Rectangle(3, 4);
 console.log(calcRectangle.calculateArea());
+// ********
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    calculateArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
+const calcCircle = new Circle(5);
+console.log(calcCircle.calculateArea());
+// **********
+class AreaCalculator {
+    calculate(shape) {
+        return shape.calculateArea();
+    }
+}
+const calc = new AreaCalculator();
+const rectangle = new Rectangle(6, 6);
+const rectangleArea = calc.calculate(rectangle);
+console.log("Прямок: ", rectangleArea);
+const circle = new Circle(15);
+const circleArea = calc.calculate(circle);
+console.log("Коло: ", circleArea);
+// ********
+// class House {
+//   street: string;
+//   constructor(n: string) {
+//     this.street = n;
+//   }
+//   showAddress(this: House) {
+//     console.log("Address: " + this.street);
+//   }
+// }
+// const house = new House("Middle-earth");
+// console.log(house);
+// house.showAddress();
+// const houseCopy = { street: "Dummy", showAddress: house.showAddress };
+// houseCopy.showAddress();
+// ******
+// class A {
+//   private someProperty = "str";
+// }
+// class B extends A {
+//   showProperty() {
+//     console.log(this.someProperty); // Помилка: 'someProperty' можна викликати лише в класі A
+//   }
+// }
+// const fff = new B();
+// B.showProperty();
+// *******
+// class A {
+//   protected someProperty = "str";
+// }
+// class B extends A {
+//   showProperty() {
+//     console.log(this.someProperty); //Немає помилки
+//   }
+// }
+// const b = new B();
+// b.showProperty();
+// *********
+// застосуємо модифікатори доступу до нашого класу House:
+class House {
+    constructor(n) {
+        this.tenants = [];
+        this.street = n;
+    }
+    showAddress() {
+        console.log("Address: " + this.street);
+    }
+    addTenant(tenant) {
+        this.tenants.push(tenant);
+    }
+    showTenants() {
+        console.log(this.tenants);
+    }
+}
+const house = new House("Middle-earth");
+house.addTenant("Anton");
+house.addTenant("Nikita");
+house.showTenants(); // ["Anton", "Nikita"]
+house.tenants.push("Anton"); // Помилка: 'tenants' можна викликати лише в межах класу 'House'.
+export {};
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -18,3 +18,120 @@ class Rectangle implements Shape {
 
 const calcRectangle = new Rectangle(3, 4);
 console.log(calcRectangle.calculateArea());
+
+// ********
+
+class Circle implements Shape {
+  public radius: number;
+
+  constructor(radius: number) {
+    this.radius = radius;
+  }
+
+  calculateArea(): number {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+}
+
+const calcCircle = new Circle(5);
+console.log(calcCircle.calculateArea());
+
+// **********
+class AreaCalculator {
+  public calculate(shape: Shape): number {
+    return shape.calculateArea();
+  }
+}
+
+const calc = new AreaCalculator();
+
+const rectangle = new Rectangle(6, 6);
+const rectangleArea = calc.calculate(rectangle);
+console.log("Прямок: ", rectangleArea);
+
+const circle = new Circle(15);
+const circleArea = calc.calculate(circle);
+console.log("Коло: ", circleArea);
+
+// ********
+// class House {
+//   street: string;
+
+//   constructor(n: string) {
+//     this.street = n;
+//   }
+
+//   showAddress(this: House) {
+//     console.log("Address: " + this.street);
+//   }
+// }
+
+// const house = new House("Middle-earth");
+// console.log(house);
+// house.showAddress();
+
+// const houseCopy = { street: "Dummy", showAddress: house.showAddress };
+// houseCopy.showAddress();
+
+// ******
+
+// class A {
+//   private someProperty = "str";
+// }
+
+// class B extends A {
+//   showProperty() {
+//     console.log(this.someProperty); // Помилка: 'someProperty' можна викликати лише в класі A
+//   }
+// }
+// const fff = new B();
+// B.showProperty();
+
+// *******
+// class A {
+//   protected someProperty = "str";
+// }
+
+// class B extends A {
+//   showProperty() {
+//     console.log(this.someProperty); //Немає помилки
+//   }
+// }
+
+// const b = new B();
+// b.showProperty();
+
+// *********
+// застосуємо модифікатори доступу до нашого класу House:
+class House {
+  private street: string;
+
+  private tenants: string[] = [];
+
+  constructor(n: string) {
+    this.street = n;
+  }
+
+  public showAddress(this: House) {
+    console.log("Address: " + this.street);
+  }
+
+  public addTenant(tenant: string) {
+    this.tenants.push(tenant);
+  }
+
+  public showTenants() {
+    console.log(this.tenants);
+  }
+}
+
+const house = new House("Middle-earth");
+
+house.addTenant("Anton");
+house.addTenant("Nikita");
+
+house.showTenants(); // ["Anton", "Nikita"]
+
+house.tenants.push("Anton"); // Помилка: 'tenants' можна викликати лише в межах класу 'House'.
+
+export {};
