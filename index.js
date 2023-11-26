5d41495be6f217d42887073284a8577dd736bebc
unknown <rubikonspace@ukr.net>
2023-11-26 12:23:45 +0200

    3

--- index.js
+++ index.js
@@ -94,5 +94,25 @@ console.log("Коло: ", circleArea);
 // house.addTenant("Nikita");
 // house.showTenants(); // ["Anton", "Nikita"]
 // house.tenants.push("Anton"); // Помилка: 'tenants' можна викликати лише в межах класу 'House'.
+// abstract class Plane {
+//   protected pilotInCabin = false;
+//   public sitInPlane() {
+//     this.pilotInCabin = true;
+//   }
+//   public abstract startEngine(): boolean;
+// }
+// class Maize extends Plane {
+//   public startEngine() {
+//     // Запускаємо винти двигуна.
+//     return true;
+//   }
+// }
+// class Boeing extends Plane {
+//   public startEngine() {
+//     // Розігріваємо реактивні турбіни.
+//     return true;
+//   }
+// }
+// class BrokenPlane extends Plane {} // Error: Клас 'BrokenPlane' не реалізовує унаслідований абстрактний метод 'startEngine' з класу 'Plane'.
 // export {};
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -134,4 +134,32 @@ console.log("Коло: ", circleArea);
 
 // house.tenants.push("Anton"); // Помилка: 'tenants' можна викликати лише в межах класу 'House'.
 
+// abstract class Plane {
+//   protected pilotInCabin = false;
+
+//   public sitInPlane() {
+//     this.pilotInCabin = true;
+//   }
+
+//   public abstract startEngine(): boolean;
+// }
+
+// class Maize extends Plane {
+//   public startEngine() {
+//     // Запускаємо винти двигуна.
+
+//     return true;
+//   }
+// }
+
+// class Boeing extends Plane {
+//   public startEngine() {
+//     // Розігріваємо реактивні турбіни.
+
+//     return true;
+//   }
+// }
+
+// class BrokenPlane extends Plane {} // Error: Клас 'BrokenPlane' не реалізовує унаслідований абстрактний метод 'startEngine' з класу 'Plane'.
+
 // export {};
